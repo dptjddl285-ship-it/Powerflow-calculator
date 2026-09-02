3b7c2a91b8010a8a809f1d57426c8df72a6732f9
dptjddl285-ship-it <dptjddl285-ship-it@users.noreply.github.com>
2026-09-02 14:39:56 +0900

    fix: add missing ImagePicker and typed_data imports to main.dart

--- frontend_app/lib/main.dart
+++ frontend_app/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 import 'dart:convert'; 
+import 'dart:typed_data';
 import 'package:http/http.dart' as http; 
+import 'package:image_picker/image_picker.dart';
 
 import 'screens/review_page.dart';
 
